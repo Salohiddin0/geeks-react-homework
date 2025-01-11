b1667713140e17885a179acfcd7310565bdb5a85
Your GitHub Username <Your GitHub Email>
2025-01-11 15:07:34 +0500

    3 section tugadi

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
 import styled from 'styled-components'
 import Image1 from '../images/i1.svg'
+import Image2 from '../images/i2.svg'
+import Image3 from '../images/i3.svg'
+import Image4 from '../images/i4.svg'
 const Main = () => {
   const MainSection = styled.section`
     background-color: #e2e8f0;
@@ -23,6 +26,7 @@ const Main = () => {
     margin-bottom: 4rem;
   `
   const Card1 = styled.div`
+    border-radius: 10px;
     padding: 3rem;
     background-color: #ffffff;
   `
@@ -32,7 +36,27 @@ const Main = () => {
     border-radius: 50%;
     background-color: blue;
   `
-  const Introduction = [
+  const Header = styled.section``
+
+  const Span1 = styled.span`
+    color: black;
+    font-weight: bold;
+    margin-right: 1rem;
+  `
+  const Span2 = styled.span`
+    font-weight: bold;
+    margin-right: 1rem;
+  `
+  const Span3 = styled.span`
+    font-weight: bold;
+  `
+  const Secttion2 = styled.section``
+
+  const Link = styled.a``
+
+  const CardP = styled.p``
+
+  const CardValues = [
     {
       id: 1,
       title: 'Introduction to JavaScript',
@@ -41,11 +65,45 @@ const Main = () => {
       hour: '1 HOUR 12 MIN',
       isFree: true,
       p: 'In et tempus dui, in porta dolor. Donec molestie a purus ut interdum. Donec quis felis dignissim, luctus libero ornare',
-      Ateg: 'View Chapter Details +'
+      link: 'View Chapter Details +',
+      image: Image1
+    },
+    {
+      id: 2,
+      title: 'Variables and Constants',
+      Course: 'Courses - 3 ',
+      lesson: '8 Lessons',
+      hour: '10 MIN',
+      isFree: true,
+      p: 'Aliquam pulvinar eros a dictur vitae diam imperdiet, ornare turpis vequet elit nec, imperdiet lectuna liquam qs.',
+      link: 'View Chapter Details +',
+      image: Image2
+    },
+    {
+      id: 3,
+      title: 'JavaScript Beginning',
+      Course: 'Courses - 2',
+      lesson: '4 Lessons',
+      hour: '32 MIN',
+      isFree: true,
+      p: 'Malesuada fames ac turpis egesta mpor tempus tincidunt. Aliquam congue lacus ac tellus consectetur malesuada.',
+      link: 'View Chapter Details +',
+      image: Image3
+    },
+    {
+      id: 4,
+      title: 'Types and Operators',
+      Course: 'Courses - 4',
+      lesson: '10 Lessons',
+      hour: '32 MIN',
+      isFree: true,
+      p: 'In lobortis quam eu augue spendisse imperdiet nec orci ipsum, tempus pharetra posuere imperdiet, lacinia a nisl',
+      link: 'View Chapter Details +',
+      image: Image4
     }
   ]
 
-  const InnerH4 = styled.h4``
+  const InnerH3 = styled.h3``
   return (
     <MainSection>
       <Container>
@@ -55,19 +113,32 @@ const Main = () => {
           Vanilla JS is a fast, lightweight, cross-platformframework for
           building incredible, powerful JavaScript applications.
         </InnerP1>
-        <Row>
-          <Col {...{ md: 6 }}>
-            <Card1 className='d-flex align-items-center '>
-              <ImageDiv className='me-4'>
-                <img src={Image1} alt='' />
-              </ImageDiv>
-            </Card1>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </Col>
+        <Row className='g-4'>
+          {CardValues.map(card => {
+            const { id, title, Course, lesson, hour, isFree, p, link, image } =
+              card
+            return (
+              <>
+                <Col {...{ md: 6 }}>
+                  <Card1 className='d-flex align-items-center'>
+                    <ImageDiv className='me-4'>
+                      <img src={image} alt='' />
+                    </ImageDiv>
+                    <Header className=''>
+                      <InnerH3>{title}</InnerH3>
+                      <Span1>{Course}</Span1>
+                      <Span2 className='text-black-50'>{lesson}</Span2>
+                      <Span3 className='text-black-50'>{hour}</Span3>
+                      <Secttion2 className=''>
+                        <CardP className='text-black-50'>{p}</CardP>
+                        <Link>{link}</Link>
+                      </Secttion2>
+                    </Header>
+                  </Card1>
+                </Col>
+              </>
+            )
+          })}
         </Row>
       </Container>
     </MainSection>
